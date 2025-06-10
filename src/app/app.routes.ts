556b8e33a75ad4950172d89a4bdab77f335c1e38
Lafiasabigado <lafiasabigado97@gmail.com>
2025-06-10 11:17:29 +0100

    Ajout de formation , contact

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
+import { ContactComponent } from './contact/contact.component';
+import { FormationsComponent } from './formations/formations.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: AccueilComponent
+    },
+    {
+        path: 'contact',
+        component: ContactComponent
+    },
+    {
+        path: 'formations',
+        component: FormationsComponent
     }
 ];
